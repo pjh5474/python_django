415eccefbd90265edda9d82f5dac1960620210e7
pjh5474 <warwarsn@gmail.com>
2024-04-29 14:51:43 +0900

    Give Me Baby One More API - Authentication
    UsernameAuthentication라는 이름의 authentication class를 빌드하세요.
    UsernameAuthentication 는 반드시 BaseAuthentication에서 extend 되어야 합니다.
    X-USERNAME 헤더를 사용하는 유저를 찾으세요.

--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,6 @@
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
 from .serializers import TweetSerializer, TweetDetailSerializer
 from .models import Tweet as TweetModel
 
